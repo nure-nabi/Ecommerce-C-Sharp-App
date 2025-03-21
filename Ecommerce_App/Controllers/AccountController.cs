17b7dc1fe549c403d99d0ddacff13bfae79b9d4f
nurealways@example.com <nurealways@example.com>
2025-03-21 16:33:36 +0545

    first commit

---
+++ Ecommerce_App/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce_App.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+        public static string physicalWebRootPath = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/ServerConnection.txt");
+        public static string[] fileContents = System.IO.File.ReadAllLines(physicalWebRootPath);
+
+        string _servername = fileContents[0].ToString();
+        string _serverusername = fileContents[1].ToString();
+        string _serverpass = fileContents[2].ToString();
+        string _apiUrl = fileContents[3].ToString();
+        string _database = "PmsLite";
+
+       public AccountController()
+        { 
+
+        }
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult checkLogin(FormCollection frm) {
+            String name = frm["txtUserName"].ToString();
+            String password = frm["txtPassword"].ToString();
+            Console.WriteLine(name);
+            ViewBag.Message = "Your application .";
+            return RedirectToAction("Login");
+                
+        }
+    }
+}
---
+++ Ecommerce_App/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce_App.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PoductDetails()
+        {
+            return View();
+        }
+    }
+}
